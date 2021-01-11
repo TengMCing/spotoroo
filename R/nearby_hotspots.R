6cb5bfd3a63567a87551190c96d17857ba4d92e8
TengMCing <llreczx@gmail.com>
2021-01-11 14:11:23 +1100

    export transform_time_id()

--- R/nearby_hotspots.R
+++ R/nearby_hotspots.R
@@ -1,3 +1,16 @@
+#' Find nearby hotspots
+#'
+#' Find nearby hotspots that are included not in the list.
+#'
+#' @param hotspot_list integer; a vector of hotspot indexes.
+#' @param pointer integer; the current working index.
+#' @param lon numeric; a vector of longitude values.
+#' @param lat numeric; a vector of latitude values.
+#' @param adjDist numeric (>0); distance tolerance.
+#'                see also \code{\link{hotspot_cluster}}.
+#' @return integer; a vector of indexes of nearby hotspots. If there is no
+#'                  nearby hotspots, return \code{NULL}.
+#' @noRd
 nearby_hotspots <- function(hotspot_list, pointer, lon, lat, adjDist) {
 
   if (length(lon) == 1) return(NULL)
--- R/transform_time_id.R
+++ R/transform_time_id.R
@@ -1,3 +1,30 @@
+#' Transform observed time to time indexes
+#'
+#' Transform observed time to time indexes.
+#'
+#' The earliest observed time has a corresponding time index \code{1}. The
+#' difference between any time to the earliest observed time will be transformed
+#' using the given time unit and divided by the given time step. The final
+#' indexes will be rounded to integer.
+#'
+#' @param obsTime date/numeric; a vector of observed time of hotspots. If
+#'                              \code{timeUnit} = \code{"n"}, \code{obsTime}
+#'                              can be a numeric vector, otherwise, it needs
+#'                              to be in date time format.
+#' @param timeUnit character; the unit of time, one of "s" (secs), "m"(mins),
+#'                            "h"(hours), "d"(days) and "n"(numeric).
+#' @param timeStep numeric (>0); the number of units of timeUnit as a time
+#'                               step.
+#' @return integer; a vector of time indexes.
+#' @examples
+#' obsTime <- as.Date(c("2020-01-01",
+#'                      "2020-01-02",
+#'                      "2020-01-04"))
+#'
+#' transform_time_id(obsTime, "h", 1)
+#'
+#' # 1 25 73
+#' @export
 transform_time_id <- function(obsTime, timeUnit, timeStep) {
 
   tb <- list(s = "secs", m = "mins", h = "hours", d = "days", n = "numeric")
